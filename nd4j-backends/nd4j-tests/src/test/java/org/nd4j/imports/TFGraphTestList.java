1d0c01cdddce3ee0e66980fb32187989ddecf8c6
raver119 <raver119@gmail.com>
2017-12-06 18:56:41 +0300

    few more missing ops

--- nd4j-backends/nd4j-tests/src/test/java/org/nd4j/imports/TFGraphTestList.java
+++ nd4j-backends/nd4j-tests/src/test/java/org/nd4j/imports/TFGraphTestList.java
@@ -26,13 +26,13 @@ import static org.nd4j.imports.TFGraphTestAllHelper.*;
 public class TFGraphTestList {
 
     public static String[] modelNames = new String[]{
-            "add_n",
+            //"add_n",
             //"ae_00",
             //"bias_add",
             //"conv_0",
             //"deep_mnist", //NOTE THIS ONE WILL FAIL because it is expecting a placeholder value for dropout % which we tie to 1.0 in inference
             //"deep_mnist_no_dropout", //Takes way too long since there are a lot of nodes, would skip for now
-            //"g_00", //This has no placeholders in the graph - not sure how to exec as it gives a NPE
+            "g_00", //This has no placeholders in the graph - not sure how to exec as it gives a NPE
             //"g_01",
             //"math_mul_order",
             //"mlp_00",
