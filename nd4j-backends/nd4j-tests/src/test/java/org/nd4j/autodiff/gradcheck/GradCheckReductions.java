f4f65911699d72fafcc3a05849b25792eaa52aa9
Alex Black <blacka101@gmail.com>
2018-01-05 12:24:11 +1100

    Additional reduction gradient check

--- nd4j-backends/nd4j-tests/src/test/java/org/nd4j/autodiff/gradcheck/GradCheckReductions.java
+++ nd4j-backends/nd4j-tests/src/test/java/org/nd4j/autodiff/gradcheck/GradCheckReductions.java
@@ -21,6 +21,7 @@ public class GradCheckReductions {
 
     @Test
     public void testReductionGradientsSimple(){
+        //Test reductions: final and only function
         Nd4j.getRandom().setSeed(12345);
 
         for (int i = 0; i < 7; i++) {
@@ -81,70 +82,167 @@ public class GradCheckReductions {
 
     @Test
     public void testReductionGradients1(){
+        //Test reductions: final, but *not* the only function
         Nd4j.getRandom().setSeed(12345);
 
-        for (int i = 0; i < 7; i++) {
+        for(int dim : new int[]{0, Integer.MAX_VALUE}) {    //These two cases are equivalent here
 
-            SameDiff sd = SameDiff.create();
+            for (int i = 0; i < 7; i++) {
 
-            int nOut = 4;
-            int minibatch = 10;
-            SDVariable input = sd.var("in", new int[]{-1, nOut});
-            SDVariable label = sd.var("label", new int[]{-1, nOut});
+                SameDiff sd = SameDiff.create();
 
-            SDVariable diff = input.sub(label);
-            SDVariable sqDiff = diff.mul(diff);
-            SDVariable msePerEx = sd.mean("msePerEx", sqDiff, 1);
+                int nOut = 4;
+                int minibatch = 10;
+                SDVariable input = sd.var("in", new int[]{-1, nOut});
+                SDVariable label = sd.var("label", new int[]{-1, nOut});
 
-            SDVariable loss;
-            String name;
-            switch (i) {
-                case 0:
-                    loss = sd.mean("loss", msePerEx, 0);
-                    name = "mean";
-                    break;
-                case 1:
-                    loss = sd.sum("loss", msePerEx, 0);
-                    name = "sum";
-                    break;
-                case 2:
-                    loss = sd.standardDeviation("loss", msePerEx, true, 0);
-                    name = "stdev";
-                    break;
-                case 3:
-                    loss = sd.min("loss", msePerEx, 0);
-                    name = "min";
-                    break;
-                case 4:
-                    loss = sd.max("loss", msePerEx, 0);
-                    name = "max";
-                    break;
-                case 5:
-                    loss = sd.variance("loss", msePerEx, true, 0);
-                    name = "variance";
-                    break;
-                case 6:
-                    loss = sd.prod("loss", msePerEx, 0);
-                    name = "prod";
-                    break;
-                default:
-                    throw new RuntimeException();
+                SDVariable diff = input.sub(label);
+                SDVariable sqDiff = diff.mul(diff);
+                SDVariable msePerEx = sd.mean("msePerEx", sqDiff, 1);
+
+                SDVariable loss;
+                String name;
+                switch (i) {
+                    case 0:
+                        loss = sd.mean("loss", msePerEx, dim);
+                        name = "mean";
+                        break;
+                    case 1:
+                        loss = sd.sum("loss", msePerEx, dim);
+                        name = "sum";
+                        break;
+                    case 2:
+                        loss = sd.standardDeviation("loss", msePerEx, true, dim);
+                        name = "stdev";
+                        break;
+                    case 3:
+                        loss = sd.min("loss", msePerEx, dim);
+                        name = "min";
+                        break;
+                    case 4:
+                        loss = sd.max("loss", msePerEx, dim);
+                        name = "max";
+                        break;
+                    case 5:
+                        loss = sd.variance("loss", msePerEx, true, dim);
+                        name = "variance";
+                        break;
+                    case 6:
+                        loss = sd.prod("loss", msePerEx, dim);
+                        name = "prod";
+                        break;
+                    default:
+                        throw new RuntimeException();
+                }
+
+
+                String msg = "test: " + i + " - " + name + ", dimension=" + dim;
+                log.info("*** Starting test: " + msg);
+
+                INDArray inputArr = Nd4j.randn(minibatch, nOut).muli(100);
+                INDArray labelArr = Nd4j.randn(minibatch, nOut).muli(100);
+
+                sd.associateArrayWithVariable(inputArr, input);
+                sd.associateArrayWithVariable(labelArr, label);
+
+                boolean ok = GradCheckUtil.checkGradients(sd);
+
+
+                assertTrue(msg, ok);
             }
+        }
+    }
+
+    @Test
+    public void testReductionGradients2() {
+        //Test reductions: NON-final function
+        Nd4j.getRandom().setSeed(12345);
+
+        int d0 = 10;
+        int d1 = 4;
+        int d2 = 8;
+
+        for (int reduceDim : new int[]{0, 1, 2}) {
+            for (int i = 0; i < 7; i++) {
+
+                int[] outShape;
+                switch(reduceDim){
+                    case 0:
+                        outShape = new int[]{d1,d2};
+                        break;
+                    case 1:
+                        outShape = new int[]{d0,d2};
+                        break;
+                    case 2:
+                        outShape = new int[]{d0, d1};
+                        break;
+                    default:
+                        throw new RuntimeException();
+                }
+
+                SameDiff sd = SameDiff.create();
 
 
-            String msg = "test: " + i + " - " + name;
-            log.info("*** Starting test: " + msg);
-
-            INDArray inputArr = Nd4j.randn(minibatch, nOut).muli(100);
-            INDArray labelArr = Nd4j.randn(minibatch, nOut).muli(100);
-
-            sd.associateArrayWithVariable(inputArr, input);
-            sd.associateArrayWithVariable(labelArr, label);
-
-            boolean ok = GradCheckUtil.checkGradients(sd);
+                SDVariable in = sd.var("in", new int[]{-1, d1, d2});
+//                SDVariable in = sd.var("in", new int[]{d0, d1, d2});
+                SDVariable label = sd.var("label", outShape);
+                SDVariable second = in.mul(2);
 
 
-            assertTrue(msg, ok);
+                SDVariable reduced;
+                String name;
+                switch (i) {
+                    case 0:
+                        reduced = sd.mean("reduced", second, reduceDim);
+                        name = "mean";
+                        break;
+                    case 1:
+                        reduced = sd.sum("reduced", second, reduceDim);
+                        name = "sum";
+                        break;
+                    case 2:
+                        reduced = sd.standardDeviation("reduced", second, true, reduceDim);
+                        name = "stdev";
+                        break;
+                    case 3:
+                        reduced = sd.min("reduced", second, reduceDim);
+                        name = "min";
+                        break;
+                    case 4:
+                        reduced = sd.max("reduced", second, reduceDim);
+                        name = "max";
+                        break;
+                    case 5:
+                        reduced = sd.variance("reduced", second, true, reduceDim);
+                        name = "variance";
+                        break;
+                    case 6:
+                        reduced = sd.prod("reduced", second, reduceDim);
+                        name = "prod";
+                        break;
+                    default:
+                        throw new RuntimeException();
+                }
+
+                SDVariable add = reduced.add(1.0);
+
+                SDVariable diff = label.sub(add);
+                SDVariable sqDiff = diff.mul(diff);
+                SDVariable mseLoss = sd.mean("loss", sqDiff);
+
+
+                String msg = "test: " + i + " - " + name + ", dimension=" + reduceDim;
+                log.info("*** Starting test: " + msg);
+
+                INDArray inputArr = Nd4j.randn(new int[]{d0,d1,d2}).muli(100);
+                INDArray labelArr = Nd4j.randn(outShape).muli(100);
+                sd.associateArrayWithVariable(inputArr, in);
+                sd.associateArrayWithVariable(labelArr, label);
+
+                boolean ok = GradCheckUtil.checkGradients(sd);
+
+                assertTrue(msg, ok);
+            }
         }
     }
 }
