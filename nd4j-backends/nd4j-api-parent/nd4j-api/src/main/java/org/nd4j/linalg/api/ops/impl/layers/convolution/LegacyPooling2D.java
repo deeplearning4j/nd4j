68f003c691dbedb8336ec7a2eb3e2fc6274e6961
Alex Black <blacka101@gmail.com>
2018-02-01 18:58:18 +1100

    Re-add old pooling op as LegacyPooling2D

---
+++ nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/api/ops/impl/layers/convolution/LegacyPooling2D.java
@@ -0,0 +1,118 @@
+package org.nd4j.linalg.api.ops.impl.layers.convolution;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.nd4j.autodiff.samediff.SDVariable;
+import org.nd4j.linalg.api.buffer.DataBuffer;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.api.ops.BaseTransformOp;
+import org.nd4j.linalg.convolution.Convolution;
+import org.nd4j.linalg.factory.Nd4j;
+
+import java.util.List;
+
+
+/**
+ * Pooling2D operation
+ */
+@Slf4j
+public class LegacyPooling2D extends BaseTransformOp {
+
+    public enum Pooling2DType {
+        MAX, AVG, PNORM,
+    }
+
+    private int kh, kw, sy, sx, ph, pw, dh, dw;
+    private Pooling2DType type;
+    boolean isSameMode;
+    double extra;
+    @Getter protected DataBuffer im2colShape;
+
+    public LegacyPooling2D() {}
+
+    public LegacyPooling2D(INDArray x, int kh, int kw, int sy, int sx, int ph, int pw, int dh, int dw, boolean isSameMode,
+                     Pooling2DType type, double extra, INDArray z) {
+        super(x);
+
+        int outHeight = Convolution.outputSize(x.size(2), kh, sy, ph, dh, isSameMode);
+        int outWidth = Convolution.outputSize(x.size(3), kw, sx, pw, dw, isSameMode);
+
+        this.kh = kh;
+        this.kw = kw;
+        this.sy = sy;
+        this.sx = sx;
+        this.ph = ph;
+        this.pw = pw;
+        this.dh = dh;
+        this.dw = dw;
+        this.isSameMode = isSameMode;
+        this.type = type;
+        this.z = z;
+        this.extra = extra;
+        this.im2colShape = getIm2ColShape(x, kh, kw, outHeight, outWidth);
+        extraArgs = this.extraArgs();
+    }
+
+    @Override
+    public boolean isExecSpecial() {
+        return true;
+    }
+
+    @Override
+    public int opNum() {
+        return 71;
+    }
+
+    @Override
+    public Object[] extraArgs() {
+        return new Object[] {kh, kw, sy, sx, ph, pw, dh, dw, isSameMode ? 1.0 : 0.0, type.ordinal(), extra};
+    }
+
+    private static INDArray getNewOutputArray(INDArray img, int kernelHeight, int kernelWidth, int strideY, int strideX,
+                                              int padHeight, int padWidth, int dh, int dw, boolean isSameMode) {
+
+        // FIXME!!!
+
+        //number of images
+        int n = img.size(0);
+        //number of channels (depth)
+        int c = img.size(1);
+        //image height
+        int h = img.size(2);
+        //image width
+        int w = img.size(3);
+        int outHeight = Convolution.outputSize(h, kernelHeight, strideY, padHeight, dh, isSameMode);
+        int outWidth = Convolution.outputSize(w, kernelWidth, strideX, padWidth, dw, isSameMode);
+
+        return Nd4j.createUninitialized(new int[] {n, c, kernelHeight, kernelWidth, outHeight, outWidth}, 'c');
+    }
+
+    private static DataBuffer getIm2ColShape(INDArray img, int kernelHeight, int kernelWidth, int outHeight, int outWidth) {
+        //number of images
+        int n = img.size(0);
+        //number of channels (depth)
+        int c = img.size(1);
+        //image height
+        int h = img.size(2);
+        //image width
+        int w = img.size(3);
+
+        return Nd4j.getShapeInfoProvider().createShapeInformation(new int[] {n, c,  kernelHeight, kernelWidth, outHeight, outWidth}, 'c').getFirst();
+    }
+
+    @Override
+    public String onnxName() {
+        throw new UnsupportedOperationException("Not supported");
+    }
+
+    @Override
+    public String tensorflowName() {
+        throw new UnsupportedOperationException("Not supported");
+    }
+
+    @Override
+    public List<SDVariable> doDiff(List<SDVariable> f1) {
+        throw new UnsupportedOperationException("Not supported");
+    }
+
+}
