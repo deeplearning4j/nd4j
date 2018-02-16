35783fc5552fc925a232dfff04a0d9ddf1f2eda1
Max Pumperla <max.pumperla@googlemail.com>
2018-02-16 17:56:44 +0100

    samediff: cross

--- nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/api/ops/impl/shape/Cross.java
+++ nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/api/ops/impl/shape/Cross.java
@@ -1,8 +1,21 @@
 package org.nd4j.linalg.api.ops.impl.shape;
 
+import org.nd4j.autodiff.samediff.SDVariable;
+import org.nd4j.autodiff.samediff.SameDiff;
 import org.nd4j.linalg.api.ops.DynamicCustomOp;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Cross extends DynamicCustomOp {
+
+    public Cross() {
+    }
+
+
+    public Cross(SameDiff sameDiff, SDVariable[] args) {
+        super(null, sameDiff, args, false);
+    }
 
     @Override
     public String opName() {
@@ -15,5 +28,23 @@ public class Cross extends DynamicCustomOp {
         return "Cross";
     }
 
+    @Override
+    public List<SDVariable> doDiff(List<SDVariable> gradients) {
+        /**
+         * dL / dx = dL / dCross * dCross / dx
+         * dCross(a,b) / da = Cross(1, b)
+         * dCross(a,b) / db = Cross(a, 1)
+         *
+         * return (grad * Cross(1, b), grad * Cross(a, 1)
+         */
+        SDVariable grad = gradients.get(0);
+        SDVariable a = larg();
+        SDVariable b = rarg();
+        SDVariable ones = sameDiff.onesLike(a);
 
+        SDVariable gradLeft = grad.mul(sameDiff.cross(ones, b));
+        SDVariable gradRight = grad.mul(sameDiff.cross(a, ones));
+
+        return Arrays.asList(gradLeft, gradRight);
+    }
 }
