e7ab0a24ed20ed6517801b8ed772afdd4c25cb87
Alex Black <blacka101@gmail.com>
2018-04-23 16:52:50 +1000

    Add base legacy deserializer

---
+++ nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/serde/json/BaseLegacyDeserializer.java
@@ -0,0 +1,62 @@
+package org.nd4j.serde.json;
+
+import org.nd4j.shade.jackson.core.JsonParser;
+import org.nd4j.shade.jackson.databind.DeserializationContext;
+import org.nd4j.shade.jackson.databind.JsonDeserializer;
+import org.nd4j.shade.jackson.databind.JsonNode;
+import org.nd4j.shade.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+public abstract class BaseLegacyDeserializer<T> extends JsonDeserializer<T> {
+
+    public abstract Map<String,String> getLegacyNamesMap();
+
+    public abstract ObjectMapper getLegacyJsonMapper();
+
+    @Override
+    public T deserialize(JsonParser jp, DeserializationContext deserializationContext) throws IOException {
+        //Manually parse old format
+        JsonNode node = jp.getCodec().readTree(jp);
+
+        Iterator<Map.Entry<String,JsonNode>> nodes = node.fields();
+        //For legacy format, ex
+
+        List<Map.Entry<String,JsonNode>> list = new ArrayList<>();
+        while(nodes.hasNext()){
+            list.add(nodes.next());
+        }
+
+        if(list.size() != 1){
+            throw new IllegalStateException("Expected size 1: " + list.size());
+        }
+
+        String name = list.get(0).getKey();
+        JsonNode value = list.get(0).getValue();
+
+        String layerClass = getLegacyNamesMap().get(name);
+        if(layerClass == null){
+            throw new IllegalStateException("Cannot deserialize: " + name);
+        }
+
+        Class<? extends T> lClass;
+        try {
+            lClass = (Class<? extends T>) Class.forName(layerClass);
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
+
+        ObjectMapper m = getLegacyJsonMapper();
+
+        String nodeAsString = value.toString();
+        T t = m.readValue(nodeAsString, lClass);
+        return t;
+    }
+
+
+
+}
